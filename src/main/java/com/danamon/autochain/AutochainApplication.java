cc227cb2cffe35a1675bcd755a4a0f32799fff16
rizdaagisa <rizdaagisa@gmail.com>
2023-12-07 22:54:47 +0700

    INITIAL COMMIT

---
+++ src/main/java/com/danamon/autochain/AutochainApplication.java
@@ -0,0 +1,11 @@
+package com.danamon.autochain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutochainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AutochainApplication.class, args);
+	}
+}
---
+++ src/test/java/com/danamon/autochain/AutochainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danamon.autochain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutochainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
