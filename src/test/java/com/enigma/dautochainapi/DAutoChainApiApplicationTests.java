d24173b68213561b44b743e898f58210becc3886
Erwin Perdana <winp2807@gmail.com>
2023-12-08 21:05:48 +0700

    Initial Project

---
+++ src/test/java/com/enigma/dautochainapi/DAutoChainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.dautochainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAutoChainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
