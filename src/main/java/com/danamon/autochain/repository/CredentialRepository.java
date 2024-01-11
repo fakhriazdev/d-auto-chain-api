261d128fc95ec1bae0bd71965f863f28a10dee65
Wirya Muhammad Nugraha <wiryamn08@gmail.com>
2024-01-12 01:42:35 +0700

    [Fix] => fix bug in Back Office User Management

--- src/main/java/com/danamon/autochain/repository/CredentialRepository.java
+++ src/main/java/com/danamon/autochain/repository/CredentialRepository.java
@@ -1,5 +1,6 @@
 package com.danamon.autochain.repository;
 
+import com.danamon.autochain.constant.ActorType;
 import com.danamon.autochain.entity.Credential;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,5 +19,6 @@ public interface CredentialRepository extends JpaRepository<Credential, String> 
             "SELECT c FROM Credential c join UserRole ur on ur.credential = c join Roles r on ur.role = r where r.roleName = ?1 and c.actor ='BACKOFFICE'"
     )
     Page<Credential> getCredentialByActorAndRoles(String roleType , Pageable pageable);
+    Page<Credential> findByActor(ActorType actor, Pageable pageable);
 
 }
