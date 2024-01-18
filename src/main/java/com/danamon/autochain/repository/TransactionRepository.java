f319fe988dac766344452e9d1473aa2cdaefe0a9
rizdaagisa <rizdaagisa@gmail.com>
2024-01-18 18:45:25 +0700

    [DELETE] transaction

--- src/main/java/com/danamon/autochain/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.danamon.autochain.repository;
-
-import com.danamon.autochain.entity.TransactionDanamon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<TransactionDanamon, String> {
-}
